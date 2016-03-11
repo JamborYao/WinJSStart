82f543c83e3e9bbad62daa6af44b2200f519b719
Jambor <jamboryao@hotmail.com>
2016-03-11 13:29:42 +0800

    3/11 check in

--- WinJSStart/js/default.js
+++ WinJSStart/js/default.js
@@ -1,28 +1,54 @@
-﻿// For an introduction to the Blank template, see the following documentation:
+﻿/// <reference path="../page1.html" />
+/// <reference path="../page1.html" />
+// For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkId=232509
 (function () {
-	"use strict";
+    "use strict";
 
-	var app = WinJS.Application;
-	var activation = Windows.ApplicationModel.Activation;
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
 
-	app.onactivated = function (args) {
-		if (args.detail.kind === activation.ActivationKind.launch) {
-			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-				// TODO: This application has been newly launched. Initialize your application here.
-			} else {
-				// TODO: This application was suspended and then terminated.
-				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
-			}
-			args.setPromise(WinJS.UI.processAll());
-		}
-	};
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: This application has been newly launched. Initialize your application here.
+                var test = 1;
+            } else {
+                var test = 1;
 
-	app.oncheckpoint = function (args) {
-		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
-		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
-		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
-	};
+                // TODO: This application was suspended and then terminated.
+                // To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+            }
+           
 
-	app.start();
+
+
+            args.setPromise(WinJS.UI.processAll().then(
+                function ()
+                {
+                   WinJS.Navigation.navigate("page1/page1.html");
+                }
+                ));
+
+          
+
+
+        }
+      
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+        // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+        // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+        var test = 1;
+    };
+
+    app.start();
+
+  
+
+
+
+
 })();
